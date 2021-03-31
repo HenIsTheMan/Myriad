8e39c50465b053bf1c24d7c6ed6d061064ab590e
HenIsTheMan <193541T@mymail.nyp.edu.sg>
2021-03-31 12:26:47 +0800

    Long press

--- scripts/MtlControl.ts
+++ scripts/MtlControl.ts
@@ -2,13 +2,13 @@ import TouchGestures from 'TouchGestures';
 import Materials from 'Materials'
 import Scene from 'Scene';
 
-(async function () {  // Enables async/await in JS [part 1]
+(async function () {
     var currIndex = 0;
 
     const rect = await Scene.root.findFirst('Rect') as Mesh;
     const mtls = await Materials.getAll() as MaterialBase[];
 
-    TouchGestures.onTap(rect).subscribe((event: TapGesture) => {
+    TouchGestures.onLongPress(rect).subscribe((event: LongPressGesture) => {
         if(currIndex == mtls.length - 1) {
             currIndex = 0;
         } else {
