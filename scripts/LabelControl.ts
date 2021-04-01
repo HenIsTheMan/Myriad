e5bbd948ae9a274e1f7dd092a347e54c95227ad2
HenIsTheMan <193541T@mymail.nyp.edu.sg>
2021-04-02 01:52:33 +0800

    yes

--- scripts/LabelControl.ts
+++ scripts/LabelControl.ts
@@ -14,13 +14,13 @@ function Lerp(start: number, end: number, lerpFactor: number): number {
 export function LabelChange(labelMesh: Mesh, labelTextMesh: Mesh, rect: Mesh): void {
     var text: string;
 
-    rect.getMaterial().then((rectMtl: MaterialBase) => {
+    rect.getMaterial().then((rectMtl: MaterialBase): void => {
         text = rectMtl.name;
         text = text.substr(0, text.length - 3);
 
         var limit: number = text.length;
-        for (var i: number = 1; i < limit; ++i) {
-            if (text[i] == text[i].toUpperCase()) { //If UpperCase...
+        for(var i: number = 1; i < limit; ++i) {
+            if(text[i] == text[i].toUpperCase()) { //If UpperCase...
                 text = text.substring(0, i) + ' ' + text.substring(i, limit);
                 ++limit;
                 i += 2;
@@ -48,10 +48,10 @@ export function LabelChange(labelMesh: Mesh, labelTextMesh: Mesh, rect: Mesh): v
 
             currAlpha = Lerp(startAlpha, endAlpha, Math.min(1, animTime / animDuration));
 
-            labelMesh.getMaterial().then((myMtl: MaterialBase) => {
+            labelMesh.getMaterial().then((myMtl: MaterialBase): void => {
                 myMtl.opacity = Reactive.val(currAlpha);
             });
-            labelTextMesh.getMaterial().then((myMtl: MaterialBase) => {
+            labelTextMesh.getMaterial().then((myMtl: MaterialBase): void => {
                 myMtl.opacity = Reactive.val(currAlpha);
             });
 
@@ -64,14 +64,14 @@ export function LabelChange(labelMesh: Mesh, labelTextMesh: Mesh, rect: Mesh): v
     BlockingAction.startCoroutine(MyRoutine);
 }
 
-(async function () {
+(async function(): Promise<void> {
     const canvas: Canvas = await Scene.root.findFirst('Canvas') as Canvas;
     const label: Canvas = await Scene.root.findFirst('Label') as Canvas;
 
     label.transform.x = canvas.width.mul(0.5).sub(label.bounds.width.mul(0.5));
     label.transform.y = canvas.height.mul(0.5).sub(label.bounds.height.mul(0.5));
 
-    const rect = await Scene.root.findFirst('Rect') as Mesh;
+    const rect: Mesh = await Scene.root.findFirst('Rect') as Mesh;
     const labelMesh: Mesh = await Scene.root.findFirst('Label') as Mesh;
     const labelTextMesh: Mesh = await Scene.root.findFirst('LabelText') as Mesh;
 
--- scripts/MtlControl.ts
+++ scripts/MtlControl.ts
@@ -8,15 +8,15 @@ import {
 
 export var currIndex: number = 0;
 
-(async function () {
-    const rect = await Scene.root.findFirst('Rect') as Mesh;
-    const mtls = await Materials.getAll() as MaterialBase[];
+(async function(): Promise<void> {
+    const rect: Mesh = await Scene.root.findFirst('Rect') as Mesh;
+    const mtls: MaterialBase[] = await Materials.getAll() as MaterialBase[];
     const labelMesh: Mesh = await Scene.root.findFirst('Label') as Mesh;
     const labelTextMesh: Mesh = await Scene.root.findFirst('LabelText') as Mesh;
 
-    TouchGestures.onLongPress(rect).subscribe((event: LongPressGesture) => {
+    TouchGestures.onLongPress(rect).subscribe((event: LongPressGesture): void => {
         do {
-            if (currIndex == mtls.length - 1) {
+            if(currIndex == mtls.length - 1) {
                 currIndex = 0;
             } else {
                 ++currIndex;
