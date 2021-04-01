7b0ef37afc2a45859b4971c8b37162e97ab43e31
HenIsTheMan <193541T@mymail.nyp.edu.sg>
2021-04-01 16:45:09 +0800

    space

--- scripts/LabelControl.ts
+++ scripts/LabelControl.ts
@@ -2,12 +2,27 @@ import TouchGestures from 'TouchGestures';
 import Materials from 'Materials';
 import Scene from 'Scene';
 import NativeUI from 'NativeUI';
+import Diagnostics from 'Diagnostics';
 
 import {
     currIndex
 } from './MtlControl'
 
 (async function () {
+    //* Label Bg
+    /*var screenWidth: number = 0;
+    var screenHeight: number = 0;
+
+    Scene.root.find('Canvas').bounds.height.monitor({ fireOnInitialValue: true }).subscribe(function (height) {
+        screen_height = height.newValue;
+    });
+
+    Scene.root.find('Canvas').bounds.width.monitor({ fireOnInitialValue: true }).subscribe(function (width) {
+        screen_width = width.newValue;
+    });*/
+    //*/
+
+    //* LabelText
     const rect = await Scene.root.findFirst('Rect') as Mesh;
     const mtls = await Materials.getAll() as MaterialBase[];
 
@@ -16,7 +31,16 @@ import {
 
         text = text.substr(0, text.length - 3);
 
-        NativeUI.setText("Label", text);
+        var limit: number = text.length;
+        for(var i: number = 1; i < limit; ++i) {
+            if(text[i] == text[i].toUpperCase()) { //If UpperCase...
+                text = text.substring(0, i) + ' ' + text.substring(i, limit);
+                ++limit;
+                i += 2;
+            }
+        }
+
+        NativeUI.setText("LabelText", text);
     }
 
     TextChange();
@@ -24,4 +48,5 @@ import {
     TouchGestures.onLongPress(rect).subscribe((event: LongPressGesture) => {
         TextChange();
     });
+    //*/
 })();
