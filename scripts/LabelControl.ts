9506a217242ac9ec78146c477cba783b9f71d1ad
HenIsTheMan <193541T@mymail.nyp.edu.sg>
2021-04-01 15:01:18 +0800

    TextChange();

--- scripts/LabelControl.ts
+++ scripts/LabelControl.ts
@@ -11,17 +11,17 @@ import {
     const rect = await Scene.root.findFirst('Rect') as Mesh;
     const mtls = await Materials.getAll() as MaterialBase[];
 
-    var text: string = mtls[currIndex].name;
-
-    text = text.substr(0, text.length - 3);
-
-    NativeUI.setText("Label", text);
-
-    TouchGestures.onLongPress(rect).subscribe((event: LongPressGesture) => {
+    let TextChange = (): void => {
         var text: string = mtls[currIndex].name;
 
         text = text.substr(0, text.length - 3);
 
         NativeUI.setText("Label", text);
+    }
+
+    TextChange();
+
+    TouchGestures.onLongPress(rect).subscribe((event: LongPressGesture) => {
+        TextChange();
     });
 })();
